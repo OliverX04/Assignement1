6700b69c9772cddf86d49f6cafa2f6d33f26b83f
OliverX04 <olichaparro@icloud.com>
2025-10-21 13:13:19 +0200

    added Apicontracts and webAPI  project files

--- Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efefd0a09fbf4e75aced350616803fd33154d6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7eb5a6a4165b8bc9ff175edceede78cce5596f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
